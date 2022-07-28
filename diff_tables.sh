93202232a23d1bd058c11e1211c7e9d171f92c37
Sviatoslav Sidorov <sviatoslav.sidorov@crick.ac.uk>
2022-07-28 20:29:44 +0100

    Rearranged output fields

--- diff_tables.sh
+++ diff_tables.sh
@@ -5,8 +5,10 @@ OLDRESPATH="/camp/home/sidoros/sidoros/projects/tf-splicing"
 
 echo "${NEWRESPATH}/summarise_tfs_and_domains/tables/:"
 
-echo -n "> dbd_ipr_humantfs.tsv: "
-diff ${NEWRESPATH}/summarise_tfs_and_domains/tables/dbd_ipr_humantfs.tsv ${OLDRESPATH}/analysis/dbd_ipr_humantfs.tsv | wc -l
+paste -d$'\t' \
+    <(diff ${NEWRESPATH}/summarise_tfs_and_domains/tables/dbd_ipr_humantfs.tsv ${OLDRESPATH}/analysis/dbd_ipr_humantfs.tsv | wc -l) \
+    <(echo "dbd_ipr_humantfs.tsv")
 
-echo -n "> ensg_enst_ensp_99.tsv: "
-diff ${NEWRESPATH}/summarise_tfs_and_domains/tables/ensg_enst_ensp_99.tsv ${OLDRESPATH}/data/ensembl99/ensg_enst_ensp_99.tsv | wc -l
+paste -d$'\t' \
+    <(diff ${NEWRESPATH}/summarise_tfs_and_domains/tables/ensg_enst_ensp_99.tsv ${OLDRESPATH}/data/ensembl99/ensg_enst_ensp_99.tsv | wc -l) \
+    <(echo "ensg_enst_ensp_99.tsv")
