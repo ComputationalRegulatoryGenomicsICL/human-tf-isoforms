7124c1e8d99b2afda0e358ce514cb628aaedbf44
Sviatoslav Sidorov <sviatoslav.sidorov@crick.ac.uk>
2022-07-28 20:26:06 +0100

    Fixed new lines

--- diff_tables.sh
+++ diff_tables.sh
@@ -8,7 +8,5 @@ echo "${NEWRESPATH}/summarise_tfs_and_domains/tables/:"
 echo -n "> dbd_ipr_humantfs.tsv: "
 diff ${NEWRESPATH}/summarise_tfs_and_domains/tables/dbd_ipr_humantfs.tsv ${OLDRESPATH}/analysis/dbd_ipr_humantfs.tsv | wc -l
 
-echo -ne "\n> ensg_enst_ensp_99.tsv: "
+echo -n "> ensg_enst_ensp_99.tsv: "
 diff ${NEWRESPATH}/summarise_tfs_and_domains/tables/ensg_enst_ensp_99.tsv ${OLDRESPATH}/data/ensembl99/ensg_enst_ensp_99.tsv | wc -l
-
-echo
