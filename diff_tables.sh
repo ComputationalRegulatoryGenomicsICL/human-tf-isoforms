9b5ec6671354884435de02b8a915f53acaa6a0cf
Sviatoslav Sidorov <sviatoslav.sidorov@crick.ac.uk>
2022-07-28 20:19:06 +0100

    Add wc -l after diff

--- diff_tables.sh
+++ diff_tables.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo -e "results/output_per_module/summarise_tfs_and_domains/tables/:\n"
+echo "results/output_per_module/summarise_tfs_and_domains/tables/:"
 echo -n "> dbd_ipr_humantfs.tsv: "
-diff results/output_per_module/summarise_tfs_and_domains/tables/dbd_ipr_humantfs.tsv /camp/home/sidoros/sidoros/projects/tf-splicing/analysis/dbd_ipr_humantfs.tsv
+diff results/output_per_module/summarise_tfs_and_domains/tables/dbd_ipr_humantfs.tsv /camp/home/sidoros/sidoros/projects/tf-splicing/analysis/dbd_ipr_humantfs.tsv | wc -l
